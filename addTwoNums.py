4c70ce89eda359d66d6f4724605e947642647351
aamirsohailias <amirsohail.leads@gmail.com>
2023-10-31 02:19:21 -0700

    SumOfTwoNums

---
+++ addTwoNums.py
@@ -0,0 +1,7 @@
+# taken input from user
+name = input("Enter the name: ")
+num1, num2 = input("Enter num1: "), input("Enter num2: ")
+# add two numbers
+result = int(num1) + int(num2)
+# display the result
+print(f"{name} addition of two num ({num1}) + ({num2}) is ({result}).")
